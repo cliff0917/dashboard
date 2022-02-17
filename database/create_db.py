edaaaa9914771cd718eb19383d5666773f3eeec5
cliff0917 <tcfsh210119@gmail.com>
2022-02-17 22:51:16 +0800

    Update createDB

--- database/create_db.py
+++ database/create_db.py
@@ -18,7 +18,7 @@ def createDB(database, dir_path, sudoPassword):
     # 找出年份(2000~2099)的資料夾, 並由小到大排序
     targetPattern = "20[0-9][0-9]"
     years = sorted(glob.glob(f'{dir_path}/{targetPattern}'))
-    print(years)
+    # print(years)
     months = [
         'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
         'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
@@ -33,28 +33,19 @@ def createDB(database, dir_path, sudoPassword):
         # 按照月份存取
         for month in months:
             try:
-                print(f'{year_}/{month}')
                 json_files = sorted(glob.glob(f'{year_}/{month}/*.json'))  # 找出所有日期的 .json files, 並由小到大排序
+                for json_file in json_files:
+                    f = open(json_file, 'r')
+                    lines = f.readlines()
+                    num += len(lines)
+                    try:
+                        json_lines = [json.loads(line) for line in lines]
+                    except: 
+                        pass
+                    data += json_lines
+                    print(f'{json_file} 有 {len(lines)} 筆資料')
             except: 
                 continue
-            
-            for json_file in json_files:
-                f = open(json_file, 'r')
-                lines = f.readlines()
-                num += len(lines)
-                try:
-                    json_lines = [json.loads(line) for line in lines]
-                except: 
-                    pass
-                data += json_lines
-                print(f'{json_file} 有 {len(lines)} 筆資料')
-
-            month = month_dict[month]
-            year = year_.split('/')[-1]
-            try:
-                database.insert_many(data) # insert data into mongoDB
-                print(f'* {year}年{month}月 {num} 筆')
-            except:
-                pass
-
+                
+    database.insert_many(data) # insert data into mongoDB
     print(f'總共 {num} 筆')
