85327f625f1a7325be9c238410e06d71048d04fe
cliff0917 <tcfsh210119@gmail.com>
2022-07-11 20:29:31 +0800

    add github icon & repo link

--- components/navbar.py
+++ components/navbar.py
@@ -3,6 +3,7 @@ import dash_bootstrap_components as dbc
 
 img_path = './assets/img'
 logo = f'{img_path}/logo.png'
+github=f'{img_path}/github.png'
 
 navbar = dbc.Navbar(
     [
@@ -15,7 +16,15 @@ navbar = dbc.Navbar(
                 ],
             ),
             href="https://www.ncku.edu.tw/",
-        )
+        ),
+        dbc.Col(style={'width': 4}),
+        html.A(
+            # 利用 row, col 來控制排版
+            dbc.Row(
+                dbc.Col(html.Img(src=github, height="50px", style={'background-color':'white'})),
+            ),
+            href="https://github.com/cliff0917/dashboard",
+        ),
     ],
     color="dark",
     dark=True,
